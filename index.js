1acccde6b83b954846878fc9c3a532eecef690bc
gibsonbett <gibson.bett@student.moringaschool.com>
2022-08-16 08:02:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft(){
+    dodger.style.left = "20px"
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
